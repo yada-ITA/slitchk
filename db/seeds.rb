1be46c0ccef1ed6670e8b1e6e6f83da51ebd4389
yada-ITA <yada.ita+github@gmail.com>
2015-12-28 00:43:31 +0900

    Readmeを追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ Member.create([{ name: 'サンプルユーザーその１' }, { name: 'サンプルユーザーその２' }])
